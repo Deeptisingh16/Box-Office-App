eddd6dc7dd3095ae91fee421716563259d94cbf4
Deepti Singh <deepti.singh_cs19@gla.ac.in>
2023-12-20 20:39:32 +0530

    Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,58 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { searchForShows } from '../api/tvmaze';
+import { useState } from 'react';
 
 const Home = () => {
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    try {
+      setApiDataError(null);
+
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+    // const response = await fetch(
+    //   `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    // );
+    // const body = await response.json();
+
+    // https://api.tvmaze.com/search/shows?q=girls
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error Occured: {apiDataError.message}</div>;
+    }
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
   return (
     <div>
-      <Link to="/starred">GO TO STARRED PAGE </Link>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchStr}
+          onChange={onSearchInputChange}
+        ></input>
+        <button type="submit">Search</button>
+      </form>
+      <div>{renderApiData()}</div>
+      {/* <Link to="/starred">GO TO STARRED PAGE </Link> */}
     </div>
   );
 };
