cca4a9f7355222d0973f4070ec8724df38fb3538
Deepti Singh <deepti.singh_cs19@gla.ac.in>
2023-12-21 11:29:19 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,27 @@
 // import { Link } from 'react-router-dom';
-import { searchForShows } from '../api/tvmaze';
+import { searchForShows, searchForPeople } from '../api/tvmaze';
 import { useState } from 'react';
+import SearchForm from '../components/SearchForm';
 
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
 
-  const onSearchInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
-
-  const onSearch = async ev => {
-    ev.preventDefault();
-
+  const onSearch = async ({ q, searchOption }) => {
     try {
       setApiDataError(null);
 
-      const result = await searchForShows(searchStr);
+      let result;
+
+      if (searchOption === 'shows') {
+        result = await searchForShows(q);
+      } else {
+        result = await searchForPeople(q);
+      }
       setApiData(result);
     } catch (error) {
       setApiDataError(error);
     }
-    // const response = await fetch(
-    //   `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    // );
-    // const body = await response.json();
-
-    // https://api.tvmaze.com/search/shows?q=girls
   };
 
   const renderApiData = () => {
@@ -35,22 +29,17 @@ const Home = () => {
       return <div>Error Occured: {apiDataError.message}</div>;
     }
     if (apiData) {
-      return apiData.map(data => (
-        <div key={data.show.id}>{data.show.name}</div>
-      ));
+      return apiData[0].show
+        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+        : apiData.map(data => (
+            <div key={data.person.id}>{data.person.name}</div>
+          ));
     }
     return null;
   };
   return (
     <div>
-      <form onSubmit={onSearch}>
-        <input
-          type="text"
-          value={searchStr}
-          onChange={onSearchInputChange}
-        ></input>
-        <button type="submit">Search</button>
-      </form>
+      <SearchForm onSearch={onSearch} />
       <div>{renderApiData()}</div>
       {/* <Link to="/starred">GO TO STARRED PAGE </Link> */}
     </div>
